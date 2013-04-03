4b25100b05725fba594dc69cce816bcb2e19b92f
Ndidi Alaneme <alanen01@mc-s083240.local>
2013-04-03 13:23:42 +0100

    Doctor can only move in adjacent 8 cells

--- static/script/appui/components/gameboard.js
+++ static/script/appui/components/gameboard.js
@@ -11,6 +11,8 @@ require.def("drontal/appui/components/gameboard",
         return Grid.extend({
             init: function () {
                 var self = this;
+                this._oldCol = 0;
+                this._oldRow = 0;
 
                 self._super("gameboard", 30, 30);
 
@@ -33,6 +35,58 @@ require.def("drontal/appui/components/gameboard",
                     self.removeEventListener('aftershow', appReady);
                 });
 
+                this.addEventListener('keydown', function(e) { self._onKeyDown(e); });
+
+                this.addEventListener('select', function(e) { self._onSelectCell(e); });
+            },
+
+            // Fired when 'Enter' or 'Select' is pushed
+            _onSelectCell: function(evt){
+                this._oldCol = this._selectedCol;
+                this._oldRow = this._selectedRow;
+                alert("New Doctor position!");
+            },
+
+            // Overridden function to limit the directional choices a player can make
+            _onKeyDown: function(evt) {
+                if(evt.keyCode != KeyEvent.VK_UP && evt.keyCode != KeyEvent.VK_DOWN
+                    && evt.keyCode != KeyEvent.VK_LEFT && evt.keyCode != KeyEvent.VK_RIGHT) {
+                    return;
+                }
+
+                var _newSelectedCol = this._selectedCol;
+                var _newSelectedRow = this._selectedRow;
+                var _newSelectedWidget = null;
+                do {
+                    if(evt.keyCode == KeyEvent.VK_UP) {
+                        _newSelectedRow--;
+                    } else if(evt.keyCode == KeyEvent.VK_DOWN) {
+                        _newSelectedRow++;
+                    } else if(evt.keyCode == KeyEvent.VK_LEFT) {
+                        _newSelectedCol--;
+                    } else if(evt.keyCode == KeyEvent.VK_RIGHT) {
+                        _newSelectedCol++;
+                    }
+                    if(_newSelectedCol < 0 || _newSelectedCol < this._oldCol - 1 || _newSelectedCol > this._oldCol + 1 || _newSelectedCol >= this._cols) {
+                        break;
+                    }
+                    if(_newSelectedRow < 0 || _newSelectedRow < this._oldRow - 1 || _newSelectedRow > this._oldRow + 1 || _newSelectedRow >= this._rows) {
+                        break;
+                    }
+
+                    var _newSelectedIndex = (_newSelectedRow * this._cols) + _newSelectedCol;
+                    var _widget = this._childWidgetOrder[_newSelectedIndex];
+                    if(_widget && _widget.isFocusable()) {
+                        _newSelectedWidget = _widget;
+                        break;
+                    }
+                } while(true);
+
+                if(_newSelectedWidget) {
+                    this.setActiveChildWidget(_newSelectedWidget);
+                    evt.stopPropagation();
+
+                }
             }
         });
     }
