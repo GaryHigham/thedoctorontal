364ae9b39d4a28ffcb2f22385c1a3af7e8124b19
crossw02 <crossland.w.a@gmail.com>
2013-04-03 12:48:08 +0100

    Add debug bar

--- static/script/appui/components/gamecanvas.js
+++ static/script/appui/components/gamecanvas.js
@@ -2,21 +2,23 @@ require.def("drontal/appui/components/gamecanvas",
     [
         "antie/widgets/component",
         "drontal/appui/components/statusbar",
-        "drontal/appui/components/gameboard"
-
+        "drontal/appui/components/gameboard",
+        "drontal/appui/components/debugbar"
     ],
-    function (Component, StatusBar, GameBoard) {
+    function (Component, StatusBar, GameBoard, DebugBar) {
 
         // All components extend Component
         return Component.extend({
             init: function () {
                 var statusBar = new StatusBar();
                 var gameBoard = new GameBoard();
+                var debugBar = new DebugBar();
 
                 // It is important to call the constructor of the superclass
                 this._super("gamecanvas");
 
                 this.appendChildWidget(statusBar);
+                this.appendChildWidget(debugBar);
                 this.appendChildWidget(gameBoard);
             }
         });
